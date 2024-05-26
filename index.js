14b1c563fdf68d65b85e77bdf3d959ce68b40590
Joshua210936 <93179779+Joshua210936@users.noreply.github.com>
2024-05-26 17:22:17 +0800

    changed buyhouse and main pg to handlebars

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 
+const bodyParser = require("body-parser"); //imported body-parser
+
+const exphbs = require('express-handlebars');
+
 const app = express();
 
-app.use(express.static("public"));
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended:true})); //part of body-parser import
+
+app.use(express.static(path.join(__dirname, '/public'))); 
+
+app.engine('handlebars', exphbs.engine({ //part of handlebars setup
+    layoutsDir:__dirname+'/views/layouts',
+    partialsDir:__dirname+'/views/partials'
+}));
+
+//sets apps to use handlebars engine
+app.set('view engine','handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/',function(req,res){ //home page
+    res.render('home',{layout:'main'})
+});
+
+app.get('/buyHouse',function(req,res){ //buyHouse page
+    res.render('buyHouse',{
+        layout:'main',})
+});
+
+// app.use(express.static("public"));
 
 let port = 3001;
 
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname+"/public/Home.html");
-})
+// app.get('/', (req, res)=> {
+//     res.sendFile(__dirname+"/public/Home.html")
+// });
 
 app.listen(port, ()=>{
-    console.log(`Server running on  http://localhost:${port}`);
+    console.log(`Server running on  http://localhost:${port}`)
 });
