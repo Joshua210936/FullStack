bfdbb96bb62b5b8dde72e63b7dba09b1726e6120
kavin150405 <emailkavin15@gmail.com>
2024-07-20 12:16:23 +0800

    chhanges to schedule database

--- models/schedule.js
+++ models/schedule.js
@@ -1,6 +1,9 @@
 const sequelize = require('sequelize');
-
 const db = require('../config/DBConfig');
+
+const Customer = require('../models/custUser');
+const Listed_Properties = require('../models/Listed_Properties');
+const Agent = require('../models/Agent');
 
 const Schedule = db.define('schedule', {
     schedule_id: {
@@ -9,13 +12,25 @@ const Schedule = db.define('schedule', {
         autoIncrement: true
     },
     customer_id: {
-        type : sequelize.INTEGER
+        type: sequelize.INTEGER,
+        references: {
+            model: Customer,
+            key: 'Customer_id'
+        }
     },
     agent_id: {
         type: sequelize.INTEGER,
+        references: {
+            model: Agent,
+            key: 'agent_id'
+        }
     },
-    property_id: { 
-        type: sequelize.INTEGER 
+    property_id: {
+        type: sequelize.INTEGER,
+        references: {
+            model: Listed_Properties,
+            key: 'Property_ID'
+        }
     },
     date_selected: {
         type: sequelize.DATE
@@ -24,5 +39,14 @@ const Schedule = db.define('schedule', {
         type: sequelize.TIME
     }
 });
+
+// Set up the associations
+Customer.hasMany(Schedule, { foreignKey: 'customer_id' });
+Agent.hasMany(Schedule, { foreignKey: 'agent_id' });
+Listed_Properties.hasMany(Schedule, { foreignKey: 'property_id' });
+
+Schedule.belongsTo(Customer, { foreignKey: 'customer_id' });
+Schedule.belongsTo(Agent, { foreignKey: 'agent_id' });
+Schedule.belongsTo(Listed_Properties, { foreignKey: 'property_id' });
 
 module.exports = Schedule;
